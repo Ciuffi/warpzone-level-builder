51d61583d55beb90ab78c04e935ba61cca307f6e
Giulio Rossi <ciuffi9@mac.com>
2019-11-11 22:25:48 -0800

    change to docs folder

---
+++ docs/precache-manifest.cbe6a3ed21b10fb46251a91c9c81737d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9264ed7dd3814d305c08",
+    "url": "/css/app.8e49d2a4.css"
+  },
+  {
+    "revision": "037f5a566d5916a5c5076c76a8d0c9cf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9264ed7dd3814d305c08",
+    "url": "/js/app.2ed7681e.js"
+  },
+  {
+    "revision": "ee20ae3ac8853d03ec16",
+    "url": "/js/chunk-vendors.ed98941f.js"
+  },
+  {
+    "revision": "c3c8dc910f42246c12b262651829e92b",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
